8bee97a2b83605355e4f4c02191311ca4c4cf4b8
Bombfuse <eiffeldud@gmail.com>
2023-01-22 12:49:40 -0800

    delete target files

--- target/package/emd_earcutr-0.1.0/target/debug/deps/emd_earcutr-199f2b18d0ee39d4.d
+++
@@ -1,7 +0,0 @@
-D:\gemdrop\projects\earcutr\target\package\emd_earcutr-0.1.0\target\debug\deps\emd_earcutr-199f2b18d0ee39d4.rmeta: src\lib.rs
-
-D:\gemdrop\projects\earcutr\target\package\emd_earcutr-0.1.0\target\debug\deps\libemd_earcutr-199f2b18d0ee39d4.rlib: src\lib.rs
-
-D:\gemdrop\projects\earcutr\target\package\emd_earcutr-0.1.0\target\debug\deps\emd_earcutr-199f2b18d0ee39d4.d: src\lib.rs
-
-src\lib.rs:
--- target/package/emd_earcutr-0.1.0/target/debug/libemd_earcutr.d
+++
@@ -1,1 +0,0 @@
-D:\gemdrop\projects\earcutr\target\package\emd_earcutr-0.1.0\target\debug\libemd_earcutr.rlib: D:\gemdrop\projects\earcutr\target\package\emd_earcutr-0.1.0\src\lib.rs
